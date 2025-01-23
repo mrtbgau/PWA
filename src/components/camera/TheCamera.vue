991e8a78ea783cc323f076b5979ca60ebada2e80
mrtbgau <mrt.bgau@gmail.com>
2025-01-23 18:10:49 +0100

    ✨ Add photo shooting

--- src/components/camera/TheCamera.vue
+++ src/components/camera/TheCamera.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <video ref="videoElement" autoplay muted playsinline></video>
+    <button @click="takePhoto">Prendre une photo</button>
+    <canvas ref="canvasElement" style="display: none"></canvas>
+    <img v-if="photoData" :src="photoData" alt="Captured photo" />
     <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
   </div>
 </template>
@@ -9,6 +12,8 @@
 import { onMounted, ref } from "vue";
 
 const videoElement = ref<HTMLVideoElement | null>(null);
+const canvasElement = ref<HTMLCanvasElement | null>(null);
+const photoData = ref<string | null>(null);
 const errorMessage = ref<string | null>(null);
 
 const getMediaStream = async (): Promise<MediaStream> => {
@@ -27,6 +32,22 @@ const setupMediaStream = async () => {
   } catch (error) {
     errorMessage.value = "Impossible d'accéder à la caméra ou au micro.";
     console.error("Erreur lors de l'accès au flux média :", error);
+  }
+};
+
+const drawCanvas = (canvas: HTMLCanvasElement, img: HTMLVideoElement) => {
+  const context = canvas.getContext("2d");
+  if (context) {
+    canvas.width = img.videoWidth;
+    canvas.height = img.videoHeight;
+    context.drawImage(img, 0, 0, canvas.width, canvas.height);
+  }
+};
+
+const takePhoto = () => {
+  if (videoElement.value && canvasElement.value) {
+    drawCanvas(canvasElement.value, videoElement.value);
+    photoData.value = canvasElement.value.toDataURL("image/png");
   }
 };
 
