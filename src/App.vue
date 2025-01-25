5520c339c53ff5bc55116c70f1727b03b60eb5ee
mrtbgau <mrt.bgau@gmail.com>
2025-01-25 21:00:11 +0100

    💄 Add ReloadPrompt component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import TheCamera from "./components/camera/TheCamera.vue";
+import ReloadPrompt from "./ReloadPrompt.vue";
 </script>
 
 <template>
+  <ReloadPrompt />
   <TheCamera />
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "./registerSW";
 import "./style.css";
 
 createApp(App).mount("#app");
