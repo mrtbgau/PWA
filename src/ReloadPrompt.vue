96651067f51c6009fd76020f694d0291517e26ac
mrtbgau <mrt.bgau@gmail.com>
2025-01-30 23:55:25 +0100

    💄 Change to tailwind

--- src/ReloadPrompt.vue
+++ src/ReloadPrompt.vue
@@ -10,15 +10,30 @@ async function close() {
 </script>
 
 <template>
-  <div v-if="offlineReady || needRefresh" class="pwa-toast" role="alert">
-    <div class="message">
+  <div
+    v-if="offlineReady || needRefresh"
+    class="fixed right-0 bottom-0 m-4 p-3 border border-solid border-gray-600 rounded-sm z-1 text-left shadow-sm bg-white"
+    role="alert"
+  >
+    <div class="mb-2">
       <span v-if="offlineReady"> App ready to work offline </span>
       <span v-else>
         New content available, click on reload button to update.
       </span>
     </div>
-    <button v-if="needRefresh" @click="updateServiceWorker()">Reload</button>
-    <button @click="close">Close</button>
+    <button
+      class="border border-solid border-gray-600 outline-none mr-1 rounded-xs py-1 px-2.5"
+      v-if="needRefresh"
+      @click="updateServiceWorker()"
+    >
+      Reload
+    </button>
+    <button
+      class="border border-solid border-gray-600 outline-none mr-1 rounded-xs py-1 px-2.5"
+      @click="close"
+    >
+      Close
+    </button>
   </div>
 </template>
 
@@ -35,9 +50,6 @@ async function close() {
   text-align: left;
   box-shadow: 3px 4px 5px 0 #8885;
   background-color: white;
-}
-.pwa-toast .message {
-  margin-bottom: 8px;
 }
 .pwa-toast button {
   border: 1px solid #8885;
