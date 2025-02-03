460ef823794500d24af59bcf271172c34e7d9a62
mrtbgau <mrt.bgau@gmail.com>
2025-02-03 19:23:23 +0100

    🚚 Rename src/router file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import AboutView from "./AboutView.vue";
-import HomeView from "./HomeView.vue";
-
 const routes = [
   { path: "/", component: HomeView },
   { path: "/about", component: AboutView },
 ];
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+export default router;
