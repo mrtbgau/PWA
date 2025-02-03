e0b0bc657b67acab91d545ac76579e618a93b020
mrtbgau <mrt.bgau@gmail.com>
2025-02-03 11:44:14 +0100

    💄 Add map component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import TheCamera from "./components/camera/TheCamera.vue";
+import TheMap from "./components/TheMap.vue";
 import ReloadPrompt from "./ReloadPrompt.vue";
 </script>
 
 <template>
   <ReloadPrompt />
-  <TheCamera />
+  <!-- <TheSideBar /> -->
+  <!-- <TheCamera /> -->
+  <TheMap />
 </template>
---
+++ src/components/TheMap.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="relative h-screen w-full">
+    <div class="absolute h-full w-full" ref="mapContainer"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Map, MapStyle, config } from "@maptiler/sdk";
+import "@maptiler/sdk/dist/maptiler-sdk.css";
+import { markRaw, onMounted, onUnmounted, shallowRef } from "vue";
+
+const mapContainer = shallowRef<HTMLElement | null>(null);
+const map = shallowRef<Map | null>(null);
+
+onMounted(() => {
+  config.apiKey = "VSYaC1pg0fNxelCb3b1s";
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      const initialState = { lng: longitude, lat: latitude, zoom: 14 };
+
+      if (mapContainer.value) {
+        map.value = markRaw(
+          new Map({
+            container: mapContainer.value,
+            style: MapStyle.STREETS,
+            center: [initialState.lng, initialState.lat],
+            zoom: initialState.zoom,
+          }),
+        );
+      }
+    });
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+  }
+});
+
+onUnmounted(() => {
+  map.value?.remove();
+});
+</script>
