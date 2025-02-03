cb05c86400c96613959b9815422612057a2f832c
mrtbgau <mrt.bgau@gmail.com>
2025-02-03 23:44:39 +0100

    💄 Update

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import TheCamera from "./components/camera/TheCamera.vue";
 import SideBar from "./components/Sidebar.vue";
-import TheMap from "./components/TheMap.vue";
 import ReloadPrompt from "./ReloadPrompt.vue";
 </script>
 
 <template>
   <ReloadPrompt />
-  <!-- <TheSideBar /> -->
-  <!-- <TheCamera /> -->
-  <TheMap />
-  <SideBar />
+  <main>
+    <div class="flex h-screen">
+      <SideBar />
+      <router-view />
+    </div>
+  </main>
 </template>
