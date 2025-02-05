256db8453fd0296e41dd14a81a910c79947a7d58
mrtbgau <mrt.bgau@gmail.com>
2025-02-05 14:44:20 +0100

    🚚 Change routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import { Camera, MapPin } from "lucide-vue-next";
 import TheCamera from "../components/camera/TheCamera.vue";
 import Map from "../views/Map.vue";
 
 const routes = [
-  { path: "/location", component: Map },
-  { path: "/camera", component: TheCamera },
+  {
+    path: "/location",
+    component: Map,
+    meta: { label: "Géolocalisation", icon: MapPin },
+  },
+  {
+    path: "/camera",
+    component: TheCamera,
+    meta: { label: "Caméra", icon: Camera },
+  },
 ];
 
 const router = createRouter({
