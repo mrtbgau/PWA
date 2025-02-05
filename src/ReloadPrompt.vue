898d3280fcf8fbc81878d941dcf2132fa4e9fa62
mrtbgau <mrt.bgau@gmail.com>
2025-02-05 09:43:40 +0100

    💄 Remove useless css

--- src/ReloadPrompt.vue
+++ src/ReloadPrompt.vue
@@ -36,26 +36,3 @@ async function close() {
     </button>
   </div>
 </template>
-
-<style>
-.pwa-toast {
-  position: fixed;
-  right: 0;
-  bottom: 0;
-  margin: 16px;
-  padding: 12px;
-  border: 1px solid #8885;
-  border-radius: 4px;
-  z-index: 1;
-  text-align: left;
-  box-shadow: 3px 4px 5px 0 #8885;
-  background-color: white;
-}
-.pwa-toast button {
-  border: 1px solid #8885;
-  outline: none;
-  margin-right: 5px;
-  border-radius: 2px;
-  padding: 3px 10px;
-}
-</style>
