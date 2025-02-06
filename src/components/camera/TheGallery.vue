f505ae0b9485496f7b1f3d52c17bd5bf027c91a4
mrtbgau <mrt.bgau@gmail.com>
2025-02-06 11:05:17 +0100

    💄Add gallery component

---
+++ src/components/camera/TheGallery.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-if="photoList.length">
+      <div v-for="(photo, index) in photoList" :key="index" class="photo-item">
+        <img :src="photo" alt="Photo sauvegardée" />
+        <button @click="deletePhoto(index)">Supprimer</button>
+      </div>
+    </div>
+    <p v-else>Aucune photo enregistrée.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useStorage } from "@vueuse/core";
+
+const photoList = useStorage<string[]>("photo-list", []);
+
+const deletePhoto = (index: number) => {
+  photoList.value.splice(index, 1);
+};
+</script>
