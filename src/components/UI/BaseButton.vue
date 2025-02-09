9bd111f6cde633d2be3123918c53693eec651fcc
mrtbgau <mrt.bgau@gmail.com>
2025-02-09 18:16:59 +0100

    💄 Add emit

--- src/components/UI/BaseButton.vue
+++ src/components/UI/BaseButton.vue
@@ -6,7 +6,7 @@
         ? 'border-none hover:bg-white/15'
         : 'border-3 border-solid border-violet-500 bg-violet-500 hover:border-violet-700 hover:bg-violet-700',
     ]"
-    @click="onClick"
+    @click="$emit('click')"
   >
     <slot></slot>
   </button>
