fd48e6ba2e016fa5125ee99880a5acae1e28f1b5
mrtbgau <mrt.bgau@gmail.com>
2025-02-28 09:49:47 +0100

    :lipstick: Add CallControls component for managing video call actions

---
+++ src/components/webRTC/CallControls.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="mt-4 space-y-4">
+    <div class="flex space-x-2">
+      <BaseButton @click="startWebcam" :disabled="callState.webcamStarted">
+        Lancer la caméra
+      </BaseButton>
+
+      <BaseButton
+        @click="createCall"
+        :disabled="!callState.webcamStarted || callState.callCreated"
+      >
+        Lancer le call
+      </BaseButton>
+
+      <BaseButton @click="hangup" :disabled="!callState.callCreated">
+        Raccrocher
+      </BaseButton>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { CallState } from "../../types";
+import BaseButton from "../UI/BaseButton.vue";
+
+defineProps<{
+  callState: CallState;
+}>();
+
+const emit = defineEmits<{
+  "start-webcam": [];
+  "create-call": [];
+  hangup: [];
+}>();
+
+const startWebcam = () => {
+  emit("start-webcam");
+};
+
+const createCall = () => {
+  emit("create-call");
+};
+
+const hangup = () => {
+  emit("hangup");
+};
+</script>
